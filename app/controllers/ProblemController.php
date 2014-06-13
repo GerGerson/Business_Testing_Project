ac04bbe158d6bf90f9772b71e79bac644b451878
GerGerson <jasonlai11281983@gmail.com>
2014-06-13 17:39:50 +0800

    Pass Video Infomation to Problem Page

--- app/controllers/ProblemController.php
+++ app/controllers/ProblemController.php
@@ -11,8 +11,28 @@ class ProblemController extends BaseController {
 			$numOfDir++;
 		}
 		
+		if ($event == 1){
+			$arrVideo = null;
+			$arrDesc = null;
+		}else if ($event == 2){
+			$arrVIdeo = array("2;1;http://youtu.be/CopZZr7J4w8", 
+							"5;2;http://youtu.be/y9clqejrvOw;http://youtu.be/RWvgVy1NgT8",
+							"7;1;http://youtu.be/iqTqx2l6vOc",
+							"10;2;http://youtu.be/lwrSlzH2YBk;http://youtu.be/3jqxu8mczNs",);
+			$arrDesc = array("2;1;test1",
+								"5;2;test1;test2",
+								"7;1;test1",
+								"10;2;test1;test2");
+		}else{
+			$arrVideo = null;
+			$arrDesc = null;
+		}
+		
+		
 		return View::make('problem', array('numOfProblem' => $numOfDir,
-											'event' => $event));
+											'event' => $event,
+											'arrVideo' => $arrVideo,
+											'arrDesc' => $arrDesc));
 	}
 
 }
